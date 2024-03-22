b35db62bc053d446d28b4498a0434ff2dd727f92
sofiane <129007459+NeverslowLVII@users.noreply.github.com>
2024-03-22 14:58:44 +0100

    import user module

--- InfoDIMBackend/src/app.module.ts
+++ InfoDIMBackend/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Post } from './posts/entities/post.entity';
@@ -27,6 +28,7 @@ import { Post } from './posts/entities/post.entity';
       inject: [ConfigService],
     }),
     PostsModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
